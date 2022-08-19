cbb5c036a5a56fe3013a2750814a5897a0640dbe
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-08-19 16:29:40 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const task = document.querySelector('.tasks')
+
+function addTask() {
+  let taskValue = document.getElementById('task-add').value
+  
+  if (taskValue != '') {
+    let newTask = document.createElement('li')
+    
+    // Id da nova tarefa
+    let idTask = taskValue.charAt(0) + Math.floor(Math.random() * 100000)
+    newTask.setAttribute('id', idTask)
+     
+    newTask.innerHTML = taskValue
+    
+    let span = document.createElement('span')
+    let deleteTask = document.createElement('button')
+    let taskCompleted = document.createElement('button')
+    
+    // BUTTON taskCompleted
+    taskCompleted.setAttribute('onclick', `taskCompleted(${idTask})`)
+    taskCompleted.setAttribute('class', 'btn-taskCompleted')
+    taskCompleted.innerHTML = '<i class="fa-solid fa-check"></i>'
+    
+    // BUTTON deleteTask
+    deleteTask.setAttribute('onclick', `deleteTask(${idTask})`)
+    deleteTask.setAttribute('class', 'deleteTask')
+    deleteTask.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    
+    newTask.appendChild(span)
+    span.appendChild(deleteTask)
+    span.appendChild(taskCompleted)
+    
+    task.appendChild(newTask)
+  } else {
+    alert('Digite sua tarefa')
+  }
+
+}
+
+function deleteTask(id) {
+  task.removeChild(id)
+}
+
+function taskCompleted(id) {
+ id.setAttribute('class', 'checked')
+}
